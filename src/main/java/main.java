69cb50ecef6f83421b4c6ecf25eafa5d02a0afec
max_glb <tpztpzsc@gmail.com>
2020-10-27 17:39:00 +0100

    M-1,01

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class main {
-    public static void main(String[] args){
-        System.out.println("Hallo world");
+    public static void main(String[] args) {
+      /* String test;
+       Scanner input= new Scanner(System.in);
+       while (true){
+          test=input.nextLine();
+          if("quit".equals(test)){
+              System.out.println("000000");
+              break;
+          }
+          System.out.println(test);
+       }*/
+
+        player playerA = new player();
+
+        playerA.cardarry.add(new Dragon(50, 100, 1));
+
+
+        playerA.cardarry.get(0);
+
+
+        System.out.println(playerA.cardarry.get(0).getCname() + "\n" + playerA.cardarry.get(0).getDamage());
 
     }
 }
